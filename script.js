3c1a0f3e6b53d559fd727ccc5ecd8ece9bc111c8
Mustafa Ömer ESER <mustafaomereser@gmail.com>
2021-01-03 04:45:42 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,136 @@
+// Selector //
+
+$ = function (e) {
+	try {
+		let arr;
+		if (typeof e == "string") {
+			arr = document.querySelectorAll(e);
+		} else if (typeof e == "object") {
+			return e;
+			//arr = Element.querySelectorAll([...e]);
+		}
+
+		return arr.length > 1 ? [...arr] : arr[0];
+
+	} catch (err) {
+		return undefined;
+	}
+}
+// Selector End //
+
+/*
+
+Array.prototype.name = function(className){
+	this.forEach(function(item, index){
+		console.log(item);
+	});
+	return this;
+}
+
+Object.prototype.addClass = function(className){
+	console.log(this);
+	return this;
+}
+
+Template End */
+
+
+// On //
+
+Array.prototype.on = function (_event, callback) {
+	this.forEach(function (item, index) {
+		item.addEventListener(_event, callback);
+	});
+	return this;
+}
+
+Object.prototype.on = function (_event, callback) {
+	this.addEventListener(_event, callback);
+	return this;
+}
+
+// On End //
+
+// AddClass //
+
+Array.prototype.addClass = function (className) {
+	this.forEach(function (item, index) {
+		item.classList.add(className)
+	});
+	return this;
+}
+
+Object.prototype.addClass = function (className) {
+	this.classList.add(className);
+	return this;
+}
+
+// AddClass End //
+
+// RemoveClass //
+
+Array.prototype.removeClass = function (className) {
+	this.forEach(function (item, index) {
+		item.classList.remove(className)
+	});
+	return this;
+}
+
+Object.prototype.removeClass = function (className) {
+	this.classList.remove(className);
+	return this;
+}
+
+// RemoveClass End //
+
+
+// ToggleClass //
+
+Array.prototype.toggleClass = function (className) {
+	this.forEach(function (item, index) {
+		item.classList.toggle(className);
+	});
+	return this;
+}
+
+Object.prototype.toggleClass = function (className) {
+	this.classList.toggle(className);
+	return this;
+}
+
+// ToggleClass End //
+
+// HTML //
+
+Array.prototype.html = function (html) {
+
+	if (html) {
+		this.forEach(function (item, index) {
+			item.innerHTML = html;
+		});
+	} else {
+		let items = [];
+		this.forEach(function (item, index) {
+			items[index] = item.innerHTML;
+		});
+		return items;
+
+	}
+}
+
+Object.prototype.html = function (html = undefined) {
+	if (html) {
+		return this.innerHTML = html;
+	} else {
+		return this.innerHTML;
+	}
+}
+
+// HTML End //
+
+// Each //
+Array.prototype.each = function (callback) {
+	this.forEach(callback);
+	return this;
+}
+// Each End //
